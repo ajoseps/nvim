f4ef1fe1d02522e7058ccc45b6347bc6436cda33
Arun Joseph <ajoseps@gmail.com>
2024-10-10 01:58:33 -0400

    adds color scheme and nvim-tree

--- lua/plugins/editing.lua
+++ lua/plugins/editing.lua
@@ -1,6 +1,18 @@
 return {
 	"nvim-treesitter/nvim-treesitter", build = ":TSUpdate",
 	"nvim-treesitter/playground",
-	"mbbill/undotree"
+	"mbbill/undotree",
+    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+    {
+      "nvim-tree/nvim-tree.lua",
+      version = "*",
+      lazy = false,
+      dependencies = {
+        "nvim-tree/nvim-web-devicons",
+      },
+      config = function()
+        require("nvim-tree").setup {}
+      end,
+    }
 }
 
